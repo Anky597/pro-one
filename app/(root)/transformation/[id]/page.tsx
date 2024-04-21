3315df37a5069971bd9580e957cc678afdf6d5df
Aniket Nikam <anky422003@gmail.com>
2024-04-21 15:34:46 +0530

    initital commit

---
+++ app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//local host 3000/tra/[id]
