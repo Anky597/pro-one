8ab5ce808a23887e548b68acef392103f99822e9
Aniket Nikam <anky422003@gmail.com>
2024-04-21 22:54:56 +0530

    inital commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"xPhiFMn7jvvIoovt7Hq/wShZYXwv6lhJ6UmX4yNuzhQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QEzFfbzlvWpHXnQzqRD/xuBQihNckHmu9iM8TZJb8dw=\"\n}"
